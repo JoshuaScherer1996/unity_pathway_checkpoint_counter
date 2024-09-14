fd3ffa37adf11cfc92d9b7bb559fc2ec889e798f
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-09-14 09:34:59 +0200

    importet the starter files and improved the code inside the Counter script

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    public Text counterText;
+
+    private int _count;
+
+    private void Start()
+    {
+        _count = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _count += 1;
+        counterText.text = "Count : " + _count;
+    }
+}
